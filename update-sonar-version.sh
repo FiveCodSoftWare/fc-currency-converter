259ed494b9bb49267ece9313263712f145e9b50c
Antonio Aburto <aaburto583@gmail.com>
2024-04-25 09:44:25 -0500

    fix: sh yml v

--- update-sonar-version.sh
+++ update-sonar-version.sh
@@ -3,15 +3,13 @@
 # LEANR VERSION FILE sonar-project.properties
 CURRENT_VERSION=$(grep 'sonar.projectVersion' sonar-project.properties | awk -F'=' '{print $2}')
 
-# Increment versión
+# Increment version
 NEW_VERSION=$(echo "$CURRENT_VERSION" | awk -F'.' '{$NF+=1; OFS="."; print $0}')
 
 # UPDATE FILE sonar-project.properties
-sed -i 's/sonar.projectVersion=\(.*\)/sonar.projectVersion='"$NEW_VERSION"'/g' sonar-project.properties
+sed -i "s/sonar.projectVersion=.*/sonar.projectVersion=$NEW_VERSION/" sonar-project.properties
 
-echo "New versión: $NEW_VERSION"
+echo "New version: $NEW_VERSION"
 
-# Export new version of GitHub Actions
+# Export new version for GitHub Actions
 echo "NEW_VERSION=$NEW_VERSION" >> $GITHUB_ENV
-
-
