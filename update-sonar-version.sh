7735ee1b4752a6a522463da536b0e7d1971b090b
Antonio Aburto <aaburto583@gmail.com>
2024-04-25 09:45:59 -0500

    fix: sh yml ver

--- update-sonar-version.sh
+++ update-sonar-version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # LEANR VERSION FILE sonar-project.properties
-CURRENT_VERSION=$(grep 'sonar.projectVersion' sonar-project.properties | awk -F'=' '{print $2}')
+CURRENT_VERSION=$(awk -F'=' '/sonar.projectVersion/ {print $2}' sonar-project.properties)
 
 # Increment version
 NEW_VERSION=$(echo "$CURRENT_VERSION" | awk -F'.' '{$NF+=1; OFS="."; print $0}')
 
 # UPDATE FILE sonar-project.properties
-sed -i "s/sonar.projectVersion=.*/sonar.projectVersion=$NEW_VERSION/" sonar-project.properties
+awk -v new_version="$NEW_VERSION" '/sonar.projectVersion/ {$0="sonar.projectVersion="new_version} {print}' sonar-project.properties > temp && mv temp sonar-project.properties
 
 echo "New version: $NEW_VERSION"
 
