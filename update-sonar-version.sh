e32a3fefb359624794d3880d0ca670f3396424b5
Antonio Aburto <aaburto583@gmail.com>
2024-04-25 00:18:42 -0500

    fix: version

---
+++ update-sonar-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# LEANR VERSION FILE sonar-project.properties
+CURRENT_VERSION=$(grep 'sonar.projectVersion' sonar-project.properties | awk -F'=' '{print $2}')
+
+# Increment versión
+NEW_VERSION=$(echo "$CURRENT_VERSION" | awk -F'.' '{$NF+=1; OFS="."; print $0}')
+
+# UPDATE FILE sonar-project.properties
+sed -i "s/sonar.projectVersion=.*/sonar.projectVersion=$NEW_VERSION/" sonar-project.properties
+
+echo "New versión: $NEW_VERSION"
